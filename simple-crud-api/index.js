391b515d9ab637e7e4380fa2bbb48fd425939dd8
Andressa Monteiro <dekomonte@yahoo.com.br>
2024-11-04 11:35:36 -0300

    Update CRUD-API

--- simple-crud-api/index.js
+++ simple-crud-api/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mysql = require('mysql2');
+
 const app = express()
 
 app.listen(3000,() =>{
@@ -7,4 +9,19 @@ app.listen(3000,() =>{
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API");
+});
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Flamengo_2019$@',
+  database: 'bd_ramais'
+});
+
+connection.connect((err) => {
+  if (err) {
+    return console.error('Erro ao conectar:', err.message);
+  }
+  console.log('Conectado ao MySQL com sucesso!');
 });
